4751b969761b34b1ab93e6bc19861fccae1ea7f2
ERaufi <79995615+ERaufi@users.noreply.github.com>
2023-09-30 17:15:45 +0300

    Update ListOfUpComingProjects.php

--- ListOfUpComingProjects.php
+++ ListOfUpComingProjects.php
@@ -3,7 +3,7 @@
 // // 2-Importing From Excel:(Done)
 // // 3-Exporting to Excel:(Done)
 // ! 4 Generate PDF with Laravel: (Done)
-// ! 5 laravel CRUD with Validations And Notify
+// ! 5 laravel CRUD with Validations And Notify (Done)
 // ! 6 Laravel Localization
-// ! 7 laravel CSV Export
+// ! 7 laravel CSV Export (Done)
 // ! 8 laravel Login using phone number or email
