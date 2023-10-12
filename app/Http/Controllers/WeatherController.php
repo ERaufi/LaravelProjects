b42c304a993597c9fe71efeca8012cd7e291bcb8
ERaufi <79995615+ERaufi@users.noreply.github.com>
2023-10-13 02:47:48 +0300

    Weather API

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -9,13 +9,14 @@ class WeatherController extends Controller
 {
     //
     public function index(Request $request)
+
     {
         $client = new Client();
         $response = $client->request('GET', 'http://api.openweathermap.org/data/2.5/weather', [
             'query' => [
                 'q' => $request->city,
                 'units' => $request->unit,
-                'appid' => '71755a29e2454d236cc519f604b9c99d'
+                'appid' => env('WEATHER_API')
             ]
         ]);
 
