69a6d426083158ea7964b260b3b898bd16f875ae
ERaufi <79995615+ERaufi@users.noreply.github.com>
2024-12-08 23:07:30 +0300

    "Update WeatherController to use env('WEATHER_API') for appid in index method"

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -16,7 +16,7 @@ class WeatherController extends Controller
             'query' => [
                 'q' => $request->city,
                 'units' => $request->unit,
-                'appid' => "dfgsfgsdf"
+                'appid' => env('WEATHER_API')
             ]
         ]);
 
